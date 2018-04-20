1aef44581369fb4e6a1a785fc0faa063ceec46b5
Swifty-Sousa <chso8299@colorado.edu>
2018-04-20 14:12:01 -0600

    worked on testcase maker

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CSCI2270-Final-project
 the final project for CSCI 2270
+file descriptions:
+main.cpp: 	Is the main file, takes in file as a command line argument and then reads in all data into an array. Then interacts with the user and calls all other fuctions.
+
+testcasemaker.cpp:	a file I wrote to copy paste text from the original .csv file to a new file so that I dont have to count lines and copy pasted to make my test cases.
--- testcasemaker.cpp
+++ testcasemaker.cpp
@@ -1,19 +1,51 @@
 #include<iostream>
+#include<sstream>
 #include<fstream>
+#include"HPQ.hpp"
+#include"LLPQ.hpp"
 using namespace std;
 
 int main(void)
 {
     string infile;
-    stirng outfile;
+    string outfile;
     string holder;
     int lines;
     cout<< "enter the file you would like to make a test case from"<< endl;
     getline(cin, infile);
     cout<< "enter the destination file name (if this files does not exist in the directory a new one will be made)"<< endl;
     getline(cin, outfile);
-    cout<< "enter how many lines you would like to be copied to the new file"<< end;
+    cout<< "enter how many lines you would like to be copied to the new file"<< endl;
     getline(cin, holder);
     lines=stoi(holder);
-
+    ifstream indata;
+    string data[lines];
+    indata.open(infile);
+    if(indata.fail())
+    {
+        cout<< "File: "<< infile<< " unable to be located."<< endl;
+        return 0;
+    }
+    getline(indata, holder);
+    int readcounter=1;
+    while(getline(indata, holder))
+    {
+        cout<<"reading line "<< readcounter<< " of "<< lines<< endl;
+        data[readcounter]=holder;
+        if(readcounter+1==lines)
+        {
+        cout<<"reading line "<< readcounter+1<< " of "<< lines<< endl;
+            break;
+        }
+        readcounter++;
+    }
+    indata.close();
+    cout<< "####################################"<< endl;
+    cout<< " Reading complete"<< endl;
+    cout<< endl;
+    cout<< endl;
+    cout<< endl;
+    cout<< "Wrting to file "<< outfile<< endl;
+    
+    return 0;
 }
